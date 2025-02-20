64681447dd43d054e81b3f595ad47476350ee0c8
Pedro Jesús León Solís <pedro@wisphub.net>
2025-02-20 10:37:46 -0500

    Added index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 
-const result = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
+const stripe_api_key = "sk_test_4ec39hqlyjwdarjtt1zdp7dc"
+
+const result = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto",{headers: {"x-dsi-restful":stripe_api_key}})
 
 console.log(`Hello world! ${result.data?.name}`)
